fea1ce24a11a824135fbc0a3cc14affc52aee90e
ShadowN0 <ShadowNN0000@gmail.com>
2022-02-27 17:52:45 +0100

    Added data and location objects

--- data.js
+++ data.js
@@ -2,182 +2,257 @@ locations = [
     {
         description: "You are inside a brimstone mine",
         img: "11.gif",
-        color: "rgb(235, 211, 64)"
+        color: "rgb(235, 211, 64)",
+        x: 1,
+        y: 1
     },
     {
         description: "You are at the entrance to the mine",
         img: "12.gif",
-        color: "rgb(89, 93, 87)"
+        color: "rgb(89, 93, 87)",
+        x: 2,
+        y: 1
     },
     {
         description: "A hill",
         img: "13.gif",
-        color: "rgb(117, 237, 243)"
+        color: "rgb(117, 237, 243)",
+        x: 3,
+        y: 1
     },
     {
         description: "Some bushes", 
         img: "14.gif",
-        color: "rgbz(202, 230, 51)"
+        color: "rgbz(202, 230, 51)",
+        x: 4,
+        y: 1
     },
     {
         description: "An old deserted hut",
         img: "15.gif", 
         color: "rgb(220, 204, 61)",
+        x: 5,
+        y: 1
     },
     {
         description: "The edge of a forest", 
         img: "16.gif",
-        color: "rgb(167, 245, 63)"
+        color: "rgb(167, 245, 63)",
+        x: 6,
+        y: 1
     },
     {
         description: "A dark forest",
         img: "17.gif",
-        color: "rgb(140, 253, 99)"
+        color: "rgb(140, 253, 99)",
+        x: 7,
+        y: 1
     },
     {
         description: "A man nearby making tar",
         img: "21.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 1,
+        y: 2
     },
     {
         description: "A timber yard",
         img: "22.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 2,
+        y: 2
     },
     {
         description: "You are by a roadside shrine", 
         img: "23.gif",
-        color: "rgb(167, 245, 63)"
+        color: "rgb(167, 245, 63)",
+        x: 3,
+        y: 2
     },
     {
         description: "You are by a small chapel",
         img: "24.gif",
-        color: "rgb(212, 229, 36)"
+        color: "rgb(212, 229, 36)",
+        x: 4,
+        y: 2
     },
     {
         description: "You are on a road leading to a wood",
         img: "25.gif",
-        color: "rgb(167, 245, 63)"
+        color: "rgb(167, 245, 63)",
+        x: 5,
+        y: 2
     },
     {
         description: "You are in a forest", 
         img: "26 i 65.gif",
-        color: "rgb(140, 253, 99)"
+        color: "rgb(140, 253, 99)",
+        x: 6,
+        y: 2
     },
     {
         description: "You are in a deep forest", 
         img: "27 i 67.gif",
-        color: "rgb(140, 253, 99)"
+        color: "rgb(140, 253, 99)",
+        x: 7,
+        y: 2
     },
     {
         description: "You are by the Vistula River",
         img: "31.gif",
-        color: "rgb(122, 232, 252)"
+        color: "rgb(122, 232, 252)",
+        x: 1,
+        y: 3
     },
     {
         description: "You are by the Vistula River",
         img: "32.gif", 
-        color: "rgb(140, 253, 99)"
+        color: "rgb(140, 253, 99)",
+        x: 2,
+        y: 3
     },
     {
         description: "You are on a bridge over river",
         img: "33.gif",
-        color: "rgb(108, 181, 242)"
+        color: "rgb(108, 181, 242)",
+        x: 3,
+        y: 3
     },
     {
         description: "You are by the old tavern",
         img: "34.gif",
-        color: "rgb(255, 189, 117)"
+        color: "rgb(255, 189, 117)",
+        x: 4,
+        y: 3
     },
     {
         description: "You are at the town's end",
         img: "35.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 5,
+        y: 3
     },
     {
         description: "You are in a butcher's shop",
         img: "36.gif",
-        color: "rgb(255, 188, 102)"
+        color: "rgb(255, 188, 102)",
+        x: 6,
+        y: 3
     },
     {
         description: "You are in a cooper's house",
         img: "37.gif",
-        color: "rgb(255, 188, 102)"
+        color: "rgb(255, 188, 102)",
+        x: 7,
+        y: 3
     },
     {
         description: "You are in the Wawel Castle",
         img: "41.gif",
-        color: "rgb(255, 176, 141)"
+        color: "rgb(255, 176, 141)",
+        x: 1,
+        y: 4
     },
     {
         description: "You are inside a dragon's cave",
         img: "42.gif",
-        color: "rgb(198, 205, 193)"
+        color: "rgb(198, 205, 193)",
+        x: 2,
+        y: 4
     },
     {
         description: "A perfect place to set a trap",
         img: "43.gif",
-        color: "rgb(255, 176, 141)"
+        color: "rgb(255, 176, 141)",
+        x: 3,
+        y: 4
     },
     {
         description: "You are by the water mill",
         img: "44.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 4,
+        y: 4
     },
     {
         description: "You are at a main crossroad",
         img: "45.gif",
-        color: "rgb(255, 190, 99)" 
+        color: "rgb(255, 190, 99)",
+        x: 5,
+        y: 4
     },
     {
         description: "You are on a town street",
         img: "46.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 6,
+        y: 4
     },
     {
         description: "You are in a frontyard of your house",
         img: "47.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 7,
+        y: 4
     },
     {
         description: "You are by a swift stream",
         img: "54.gif",
-        color: "rgb(108, 181, 242)"
+        color: "rgb(108, 181, 242)",
+        x: 4,
+        y: 5
     },
     {
         description: "You are on a street leading forest",
         img: "55.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 5,
+        y: 5
     },
     {
         description: "You are in a woodcutter's backyard",
         img: "56.gif",
-        color: "rgb(255, 190, 99)"
+        color: "rgb(255, 190, 99)",
+        x: 6,
+        y: 5
     },
     {
         description: "You are in a shoemaker's house",
         img: "57.gif",
-        color: "rgb(254, 194, 97)"
+        color: "rgb(254, 194, 97)",
+        x: 7,
+        y: 5
     },
     {
         description: "You are in a bleak funeral house",
         img: "64,gif",
-        color: "rgb(254, 194, 97)"
+        color: "rgb(254, 194, 97)",
+        x: 4,
+        y: 6
     },
     {
         description: "You are on a path leading to the wood",
         img: "26 i 65.gif", 
-        color: "rgb(167, 245, 63)"
+        color: "rgb(167, 245, 63)",
+        x: 5,
+        y: 6
     },
     {
         description: "You are at the edge of a forest",
         img: "66.gif",
-        color: "rgb(167, 245, 63)"
+        color: "rgb(167, 245, 63)",
+        x: 6,
+        y: 6
     },
     {
         description: "You are in a deep forest",
         img: "27 i 67.gif",
-        color: "rgb(140, 253, 99)"
+        color: "rgb(140, 253, 99)",
+        x: 7,
+        y: 6
     }
 
 ]
+
+let game_div = document.getElementById('game_div')
+let content = document.getElementById('content')
--- dratewka.js
+++ dratewka.js
@@ -1,20 +1,145 @@
 class Location{
-    constructor(description, image, color){
+    constructor(description, image, color, x, y){
         this.description = description
         this.image = image
         this.color = color
+        this.directions_to_go = []
+        this.x = x
+        this.y = y
+        this.items = []
     }
 }
 
 map = {
     location_list: [],
+    current_location: 27,
+    current_location_object: null,
 
     make_location: function(){
         let length = locations.length
         
+        for (let i = 0; i < length; i++){
+            
+            let location = new Location(locations[i].description, locations[i].img, locations[i].color, locations[i].x, locations[i].y)
+            this.location_list.push(location)
+        
+            if(location.x == 7 && location.y == 4){
+                this.location_list.push("nie ma takiej lokacji")
+                this.location_list.push("nie ma takiej lokacji")
+                this.location_list.push("nie ma takiej lokacji")
+            }
+            if(location.x == 7 && location.y == 5){
+                this.location_list.push("nie ma takiej lokacji")
+                this.location_list.push("nie ma takiej lokacji")
+                this.location_list.push("nie ma takiej lokacji")
+            }
+        }
+
+
+    },
+
+    get_current_location: function(){
+        let x = player.x
+        let y = player.y
+
+        this.current_location = (y-1)*7 + x-1
+        this.current_location_object = this.location_list[this.current_location]
+    },
+
+    render_location: function(){
+        this.remove_previous_location()
+
+        let description = this.current_location_object.description
+        let color = this.current_location_object.color
+        let img = this.current_location_object.image
+
+        let description_element = document.createElement('h2')
+        description_element.textContent = description
+
+        let img_element = document.createElement('img')
+        img_element.setAttribute('src', `./img/${img}`)
+        img_element.style.backgroundColor = color
+
+        let directions_element = document.createElement('h2')
+        //make a function that identifies attribute - directions_to_go in locations object 
+        directions_element.textContent = "You can go: WEST, NORTH, EAST, SOUTH"
+
+        let items_visible_element = document.createElement('h2')
+        if(this.current_location_object.items.length == 0){
+            items_visible_element.textContent = "You see nothing"
+        }
+        else{
+            items_visible_element.textContent = "You see:"
+            for(let i in this.current_location_object.items){
+                items_visible_element.textContent += this.current_location_object.items[i]
+            }
+        }
+
+        let player_items_element = document.createElement('h2')
+        
+        if(player.items.length == 0){
+            player_items_element.textContent = "You are carrying nothing"
+        }
+        else{
+            player_items_element.textContent = "You carry: "
+
+            for(let i in player.items){
+                player_items_element.textContent += player.items[i]
+            }
+        }
+
+        content.append(description_element)
+        content.append(img_element)
+        content.append(directions_element)
+        content.append(items_visible_element)
+        content.append(player_items_element)
+    },
+
+    remove_previous_location: function(){
+        let content_list = content.children
+        let length = content_list.length
+        console.log(content_list)
+
+        if(length > 0){
+            for(let i = length - 1; i >= 0; i--){
+                content_list[i].remove()
+            }
+        }
+        
+    },
+
+    focus_input: function(){
+        document.addEventListener('click', function(){
+            input.focus()
+        })
+        document.addEventListener('keydown', function(){
+            input.focus()
+        })
+    },
+
+    add_enter_submission: function(){
+        document.addEventListener('keydown', function(e){
+            code = e.code
+            console.log(code)
+
+            if(code == "Enter" || code == "NumpadEnter"){
+                map.command = input.value
+            }
+        })
     }
+
 }
 
+player = {
+    x: 7,
+    y: 4,
+    items: []
+} 
+
 console.log(locations.length)
-
-
+map.make_location()
+map.get_current_location()
+map.render_location()
+map.focus_input()
+map.add_enter_submission()
+console.log(map.location_list)
