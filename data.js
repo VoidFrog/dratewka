b70fb1332fc28c15e4833026de26b83585ace423
VoidFrog <ShadowNN0000@gmail.com>
2022-02-27 15:39:27 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,183 @@
+locations = [
+    {
+        description: "You are inside a brimstone mine",
+        img: "11.gif",
+        color: "rgb(235, 211, 64)"
+    },
+    {
+        description: "You are at the entrance to the mine",
+        img: "12.gif",
+        color: "rgb(89, 93, 87)"
+    },
+    {
+        description: "A hill",
+        img: "13.gif",
+        color: "rgb(117, 237, 243)"
+    },
+    {
+        description: "Some bushes", 
+        img: "14.gif",
+        color: "rgbz(202, 230, 51)"
+    },
+    {
+        description: "An old deserted hut",
+        img: "15.gif", 
+        color: "rgb(220, 204, 61)",
+    },
+    {
+        description: "The edge of a forest", 
+        img: "16.gif",
+        color: "rgb(167, 245, 63)"
+    },
+    {
+        description: "A dark forest",
+        img: "17.gif",
+        color: "rgb(140, 253, 99)"
+    },
+    {
+        description: "A man nearby making tar",
+        img: "21.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "A timber yard",
+        img: "22.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are by a roadside shrine", 
+        img: "23.gif",
+        color: "rgb(167, 245, 63)"
+    },
+    {
+        description: "You are by a small chapel",
+        img: "24.gif",
+        color: "rgb(212, 229, 36)"
+    },
+    {
+        description: "You are on a road leading to a wood",
+        img: "25.gif",
+        color: "rgb(167, 245, 63)"
+    },
+    {
+        description: "You are in a forest", 
+        img: "26 i 65.gif",
+        color: "rgb(140, 253, 99)"
+    },
+    {
+        description: "You are in a deep forest", 
+        img: "27 i 67.gif",
+        color: "rgb(140, 253, 99)"
+    },
+    {
+        description: "You are by the Vistula River",
+        img: "31.gif",
+        color: "rgb(122, 232, 252)"
+    },
+    {
+        description: "You are by the Vistula River",
+        img: "32.gif", 
+        color: "rgb(140, 253, 99)"
+    },
+    {
+        description: "You are on a bridge over river",
+        img: "33.gif",
+        color: "rgb(108, 181, 242)"
+    },
+    {
+        description: "You are by the old tavern",
+        img: "34.gif",
+        color: "rgb(255, 189, 117)"
+    },
+    {
+        description: "You are at the town's end",
+        img: "35.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are in a butcher's shop",
+        img: "36.gif",
+        color: "rgb(255, 188, 102)"
+    },
+    {
+        description: "You are in a cooper's house",
+        img: "37.gif",
+        color: "rgb(255, 188, 102)"
+    },
+    {
+        description: "You are in the Wawel Castle",
+        img: "41.gif",
+        color: "rgb(255, 176, 141)"
+    },
+    {
+        description: "You are inside a dragon's cave",
+        img: "42.gif",
+        color: "rgb(198, 205, 193)"
+    },
+    {
+        description: "A perfect place to set a trap",
+        img: "43.gif",
+        color: "rgb(255, 176, 141)"
+    },
+    {
+        description: "You are by the water mill",
+        img: "44.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are at a main crossroad",
+        img: "45.gif",
+        color: "rgb(255, 190, 99)" 
+    },
+    {
+        description: "You are on a town street",
+        img: "46.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are in a frontyard of your house",
+        img: "47.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are by a swift stream",
+        img: "54.gif",
+        color: "rgb(108, 181, 242)"
+    },
+    {
+        description: "You are on a street leading forest",
+        img: "55.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are in a woodcutter's backyard",
+        img: "56.gif",
+        color: "rgb(255, 190, 99)"
+    },
+    {
+        description: "You are in a shoemaker's house",
+        img: "57.gif",
+        color: "rgb(254, 194, 97)"
+    },
+    {
+        description: "You are in a bleak funeral house",
+        img: "64,gif",
+        color: "rgb(254, 194, 97)"
+    },
+    {
+        description: "You are on a path leading to the wood",
+        img: "26 i 65.gif", 
+        color: "rgb(167, 245, 63)"
+    },
+    {
+        description: "You are at the edge of a forest",
+        img: "66.gif",
+        color: "rgb(167, 245, 63)"
+    },
+    {
+        description: "You are in a deep forest",
+        img: "27 i 67.gif",
+        color: "rgb(140, 253, 99)"
+    }
+
+]
---
+++ dratewka.js
@@ -0,0 +1,20 @@
+class Location{
+    constructor(description, image, color){
+        this.description = description
+        this.image = image
+        this.color = color
+    }
+}
+
+map = {
+    location_list: [],
+
+    make_location: function(){
+        let length = locations.length
+        
+    }
+}
+
+console.log(locations.length)
+
+
